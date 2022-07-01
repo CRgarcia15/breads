f3f67c12443009f7d228eeeccd022925cfe43dd7
CRgarcia15 <cg58708@gmial.com>
2022-06-30 21:06:59 -0400

    sending index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -3,7 +3,11 @@ const breads = express.Router()
 const Bread = require('../models/breads.js')
 
 breads.get('/', (req, res) => {
-    res.send(Bread)
+    res.render('index',
+    {
+        breads: Bread
+    }
+    )
 })
 
 breads.get('/:arrayIndex', (req, res) => {
