c166a67a0ae58220f647b5a5e672e18c17f8f3a7
CRgarcia15 <cg58708@gmial.com>
2022-07-05 18:48:57 -0400

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+const breadsController = require('./controllers/breads_controllers.js')
 
 //MIDDLEWARE   
 app.use(express.static('public')) 
@@ -10,7 +11,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.use(express.urlencoded({extended: true}))
 app.engine('jsx', require('express-react-views').createEngine())
-
+app.use('/breads', breadsController)
 
 app.get('/', (req, res) => {
     res.send('Welcome to a bread app!')
@@ -19,9 +20,6 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
     res.send('404')
 })
-
-const breadsController = require('./controllers/breads_controllers.js')
-app.use('/breads', breadsController)
 
 app.listen(PORT, () => {
     console.log('noming at port', PORT)
