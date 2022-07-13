40d02d885e25b437da26736e36a04da8fafc9b63
CRgarcia15 <cg58708@gmial.com>
2022-07-12 21:16:13 -0400

    update

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 const breadsController = require('./controllers/breads_controllers.js')
 const methodOverride = require('method-override')
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+   () => {console.log('connected to mongo: ', process.env.MONGO_URI)} )
 
 //MIDDLEWARE   
 app.use(express.static('public')) 
 app.set('views', __dirname + '/views')
 app.use(express.urlencoded({extended: true}))
-app.use('/breads', breadsController)
 app.use(methodOverride('_method'))
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use('/breads', breadsController)
 
 app.get('/', (req, res) => {
     res.send('Welcome to a bread app!')
